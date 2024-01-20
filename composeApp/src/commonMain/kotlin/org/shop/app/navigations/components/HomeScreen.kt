b29c2081caac0636695d05b87e205ef49d8fdd9e
Khubaib Khan <18.bscs.803@gmail.com>
2024-01-20 15:47:42 +0500

    -Home Screen Implementation (#6)

--- composeApp/src/commonMain/kotlin/org/shop/app/navigations/components/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/shop/app/navigations/components/HomeScreen.kt
@@ -1,7 +1,6 @@
 package org.shop.app.navigations.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,15 +9,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LeadingIconTab
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -51,14 +55,28 @@ class HomeScreen : Screen {
                         contentDescription = null
                     )
                 }
+                IconButton(
+                    onClick = { }
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(8.dp).size(20.dp),
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null
+                    )
+                }
+                IconButton(
+                    onClick = {
+
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(8.dp).size(20.dp),
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = null,
+                    )
+                }
             }
 
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("Home")
-            }
         }
     }
 }
